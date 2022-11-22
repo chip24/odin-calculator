fafad50593c38fbb6a358a3c431cd3b713d25d24
Chip Morgan <python.chipmorgan@gmail.com>
2022-11-22 08:28:18 -0600

    Add main math functions

---
+++ script.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+    console.log(a + b);
+    return a + b;
+}
+
+function subtract (a, b) {
+    console.log(a-b);
+    return a - b;
+}
+
+function multiply (a, b) {
+    console.log(a*b);
+    return a * b;
+}
+
+function divide (a, b){
+    console.log(a/b);
+    return a / b;
+}
+
+add(4,7);
+subtract(6,8);
+multiply(3,9);
+divide(5,10);
