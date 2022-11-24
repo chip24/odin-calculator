e4e0151cf1b9067aa8f9dfbb3444ac32a1a1b2c4
Chip Morgan <python.chipmorgan@gmail.com>
2022-11-24 08:32:49 -0600

    Create HTML and CSS files and add beginning code

--- script.js
+++ script.js
@@ -22,3 +22,20 @@ add(4,7);
 subtract(6,8);
 multiply(3,9);
 divide(5,10);
+
+function operate(oper, a, b){
+    if (oper === "+"){
+        return add(a,b);        
+    }else if (oper === "-"){
+        return subtract(a,b)     
+    }else if (oper === "*"){
+        return multiply(a,b)
+    }else if(oper === "/"){
+        return divide(a,b)
+    };
+}
+
+operate("+", 3, 6);
+operate("-", 3, 6);
+operate("*", 3, 6);
+operate("/", 3, 6);
